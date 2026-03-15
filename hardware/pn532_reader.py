a63af0f9a4c43ea74724ebc156489b32240d7dd0
Kharylle Ann Iglesias <kharylleann.iglesias@cit.edu>
2026-03-15 19:16:25 +0800

    add: added code in pn532_reader file under hardware

---
+++ hardware/pn532_reader.py
@@ -0,0 +1,10 @@
+from adafruit_pn532.i2c import PN532_I2C
+
+class PN532Reader:
+
+    def __init__(self, i2c_channel):
+        self.reader = PN532_I2C(i2c_channel, debug=False)
+        self.reader.SAM_configuration()
+
+    def read_tag(self):
+        return self.reader.read_passive_target(timeout=0.5)
